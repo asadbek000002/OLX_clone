2763bf1fb2e5f29bf9795606c417d491ce3a0d87
BehruzMulkamonov <126500929+BehruzMulkamonov@users.noreply.github.com>
2023-09-23 17:29:33 +0500

    1

--- product/serializers.py
+++ product/serializers.py
@@ -1,13 +1,8 @@
 from rest_framework import serializers
 
-from .models import Category, Comment
+from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-        
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
         fields = '__all__'
