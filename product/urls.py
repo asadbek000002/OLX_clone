eb98e00a7fcdf44022edf46a8907f4cdcac03bab
Yaxshiev Farrux <129090703+Farrux9404@users.noreply.github.com>
2023-09-23 06:29:10 -0700

    urls fixed

--- product/urls.py
+++ product/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     path('product-list/',ProductViewSet.as_view() ),
     path('product-delete/<int:id>/', ProductDestroyAPIView.as_view()),
     path('product-update/<pk>/', ProductUpdateAPIView.as_view()),
+    path('saved/', SavedAPIView.as_view(), name='saved'),
+    path('delete/<int:id>/', SavedDeleteView.as_view(), name='delete'),
+    path('list/', SavedListView.as_view (), name='list'),
     
 ]
